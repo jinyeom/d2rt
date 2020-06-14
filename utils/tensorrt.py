5eda0558b7990130d43b7274453d3fa95f77467c
jinyeom <jinyeom95@gmail.com>
2020-06-14 01:18:22 -0500

    TensorRT engine build works

--- utils/tensorrt.py
+++ utils/tensorrt.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass
+from contextlib import ExitStack
 from pathlib import Path
-from typing import Union
+from typing import Union, Optional
 
 import numpy as np
 import tensorrt as trt
@@ -8,20 +8,25 @@ from pycuda import autoinit
 from pycuda import driver as cuda
 
 
-@dataclass
 class MemoryBuffer:
-    mem_dtype: np.dtype
-    host: np.ndarray
-    device: cuda.DeviceAllocation
+    def __init__(
+        self,
+        mem_dtype: np.dtype,
+        host: Optional[np.ndarray] = None,
+        device: Optional[cuda.DeviceAllocation] = None,
+    ):
+        self.mem_dtype = mem_dtype
+        self.host = host
+        self.device = device
 
 
-class TensorrtModel:
+class InferenceModel:
     def __init__(
         self,
         onnx_path: Union[str, Path],
         engine_path: Union[str, Path],
         max_batch_size: int = 1,
-        workspace_size: int = 4,
+        workspace_size: int = 1,
         fp16_mode: bool = True,
         force_rebuild: bool = False,
     ):
@@ -35,45 +40,53 @@ class TensorrtModel:
         self._init_buffers()
 
     def _init_engine(self):
-        print("initializing TensorRT engine...")
+        print("Building TensorRT engine from %s..." % self.onnx_path, flush=True)
+
         self.logger = trt.Logger(trt.Logger.Severity.INFO)
         trt.init_libnvinfer_plugins(self.logger, "")
 
         if not self.force_rebuild and self.engine_path.exists():
-            print("loading %s".format(self.engine_path))
+            print("Loading %s" % self.engine_path, flush=True)
             with trt.Runtime(self.logger) as runtime:
-                with open(path, "rb") as f:
+                with open(self.engine_path, "rb") as f:
                     engine_data = f.read()
                 self.engine = runtime.deserialize_cuda_engine(engine_data)
             return
 
-        with trt.Builder(self.logger) as builder:
+        explicit_batch = 1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
+
+        with ExitStack() as stack:
+            builder = stack.enter_context(trt.Builder(self.logger))
+            network = stack.enter_context(builder.create_network(explicit_batch))
+            parser = stack.enter_context(trt.OnnxParser(network, self.logger))
+
+            # configure builder
             builder.max_workspace_size = self.workspace_size << 30
-            builder.max_batch_size = self.batch_size
+            builder.max_batch_size = self.max_batch_size
             builder.fp16_mode = self.fp16_mode
             builder.strict_type_constraints = True
 
-            with builder.create_network() as network:
-                with trt.OnnxParser(network, trt_logger) as parser:
-                    with open(self.onnx_path, "rb") as model:
-                        success = parser.parse(model.read())
-                        if not success:
-                            err = parser.get_error(0)
-                            msg = "While parsing node number %i:\n" % err.node()
-                            msg += "%s:%i In function %s:\n[%i] %s" % (
-                                err.file(),
-                                err.line(),
-                                err.func(),
-                                err.code(),
-                                err.desc(),
-                            )
-                            raise RuntimeError(msg)
+            with open(self.onnx_path, "rb") as model:
+                success = parser.parse(model.read())
+                if not success:
+                    err = parser.get_error(0)
+                    msg = "while parsing node number %i:\n" % err.node()
+                    msg += "%s:%i In function %s:\n[%i] %s" % (
+                        err.file(),
+                        err.line(),
+                        err.func(),
+                        err.code(),
+                        err.desc(),
+                    )
+                    raise RuntimeError(msg)
 
                 self.engine = builder.build_cuda_engine(network)
                 with open(self.engine_path, "wb") as f:
                     f.write(self.engine.serialize())
 
     def _init_buffers(self):
+        print("Allocating memory buffers...", flush=True)
+
         self.stream = cuda.Stream()
         self.input_buffers = []
         self.output_buffers = []
@@ -91,3 +104,6 @@ class TensorrtModel:
             else:
                 host_mem = cuda.pagelocked_empty(size, dtype)
                 self.output_buffers.append(MemoryBuffer(dtype, host_mem, device_mem))
+
+    def __call__(self):
+        raise NotImplementedError
